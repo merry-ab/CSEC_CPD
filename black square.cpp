9e51454b7015f6a3bd1928aadfc582ecca635b5c
merry-ab <abebemariamawit@gmail.com>
2025-02-12 14:21:56 +0300

    Create black square.cpp

---
+++ black square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  string s;
+  cin>>s;
+  int count=0;
+  for(int i=0;i<s.length();i++){
+      if(s[i]=='1'){
+          count+=a;
+      }else if(s[i]=='2'){
+          count +=b;
+      }else if(s[i]=='3'){
+          count+=c;
+      }else
+         count +=d;
+  }
+  cout<<count<<endl;
+return 0;
+}
